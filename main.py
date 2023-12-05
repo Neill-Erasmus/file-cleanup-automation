6d5d68d6e3012bce519806f1f0998534c44e77d7
Neill Erasmus <neill.erasmus20@gmail.com>
2023-12-05 14:09:13 +0200

    Added the cleanup functions.

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import os, sys
 
+def ClearTemp() -> None:
+    for root, dirs, files in os.walk("C://Windows//Temp"):
+        for file in files:
+            os.remove(os.path.join(root, file))
+
+def ClearDownloads() -> None:
+    for root, dirs, files in os.walk(f"{os.path.join(os.path.expanduser('~'), 'Downloads')}"):
+        for file in files:
+            os.remove(os.path.join(root, file))
+
+def ClearRecycleBin() -> None:
+    os.system('cmd /c "echo Y|PowerShell.exe -NoProfile -Command Clear-RecycleBin"')
+
 def main() -> None:
     pass
 
